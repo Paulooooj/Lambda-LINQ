4c4904841d5df4d0e92ce680c0f6e40e9da87f57
Paulo Sergio <paulosergio_gyn@hotmail.com>
2023-03-08 10:24:59 -0300

    Projetos LinQ git Hub

---
+++ ExercicioFinalLinQ/ExercicioFinalLinQ/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Globalization;
+
+using ExercicioFinalLinQ.Entities;
+using System.Globalization;
+
+
+namespace ExercicioFinalLinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double firstSalary = double.Parse(Console.ReadLine());
+
+            try
+            {
+                List<Employee> list = new List<Employee>();
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fieds = sr.ReadLine().Split(',');
+                        string name = fieds[0];
+                        string email = fieds[1];
+                        double salary = double.Parse(fieds[2], CultureInfo.InvariantCulture);
+
+                        list.Add(new Employee(name, email, salary));
+
+                    }
+
+
+                }
+                var emails = list.Where(p => p.Salary > firstSalary).OrderBy(p => p.Email).Select(p => p.Email);
+                Console.WriteLine("Email of people whose salary is more than 2000.00:");
+
+                var sumSalary = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+                
+                foreach(string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+                Console.Write("Sum of salary of people whose name starts with 'M': " + sumSalary.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ExercicioFixacaoLinQ/ExercicioFixacaoLinQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using ExercicioFixacaoLinQ.Entities;
+using System.Globalization;
+namespace ExercicioFixacaoLinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                List<Product> list = new List<Product>();
+
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string[] fieds = sr.ReadLine().Split(',');
+                        string name = fieds[0];
+                        double price = double.Parse(fieds[1], CultureInfo.InvariantCulture);
+
+                        list.Add(new Product(name, price));
+                    }
+                }
+
+                var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+                Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+                var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+                foreach(string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
